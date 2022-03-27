20950eb88c916007eaaf15f90dedcfcfd17796b1
abelli3 <98430919+abelli3@users.noreply.github.com>
2022-03-27 12:49:18 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express()
+const router = express.Router()
+
+
+
+function signUP (user1, fname, lname, pass1, email) {
+    if (user = [NULL]){
+        print("Enter username again: ");
+    }
+    else if (fname = [NULL]){
+        print("Enter username again: ");
+    }
+    else if (lname = [NULL]){
+        print("Enter username again: ");
+    }
+    else if (pass1 = [NULL]){
+        print("Enter username again: ");
+    }
+    else if (email = [NULL]){
+        print("Enter username again: ");
+    }
+}
+
+function signIN (user2, pass2){
+
+
+}
