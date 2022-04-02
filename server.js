7874290dc377d03ca6174ed7cd35e7529044494f
Doniyor Nimatullo <47197048+ndoniyor@users.noreply.github.com>
2022-04-02 08:06:25 -0700

    sample server setup via localhost

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+
+const server = http.createServer(function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('dashboard.html', function(error, data){
+        if(error){
+            res.writeHead(404)
+            res.write('Error: File not found 0_0')
+        }
+        else{
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+server.listen(port,function(error){
+    if(error){
+        console.log('Something went wrong', error)
+    }
+    else{
+        console.log('Listening on port '+ port)
+    }
+})
